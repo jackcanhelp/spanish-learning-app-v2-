6076ec79c32b5d798c4b421b5e299bb4ac12ac22
Jack <jackinsidetaiwan@gmail.com>
2026-01-08 11:32:36 +0800

    index.html更新(三星手機自動用雲端TTS)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spanish-learning-v1.0.8'; //index.html更新(三星手機播音改善指引)
+const CACHE_NAME = 'spanish-learning-v1.0.9'; //index.html更新(三星手機自動用雲端TTS)
 const urlsToCache = [
   './',
   './index.html',
